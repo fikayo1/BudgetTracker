891a4e7a14f2064dcf7102f669331742958e899b
Oluwafikayomi Soetan <soetandan@gmail.com>
2023-07-14 02:12:59 +0100

    Updated build file

--- build.sh
+++ build.sh
@@ -2,9 +2,8 @@
 # exit on error
 set -o errexit
 
-cd source/  # Move to the Django project directory
+ # Move to the Django project directory
 
 pip install -r requirements.txt
-
 python manage.py collectstatic --no-input
 python manage.py migrate
